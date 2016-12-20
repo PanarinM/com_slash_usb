76ea7db5447e3b378b3d917dd53cafbea19ea14b
PanarinM <michaelpanarin@inbox.ru>
2016-12-20 12:03:10 +0200

    small readability tweaks

--- reader.py
+++ reader.py
@@ -14,9 +14,13 @@ if __name__ == "__main__":
         tacho_data = tacho.receive_package()
         if tacho_data[2] == 0:
             continue
-        print(tacho_data)
+        # print(tacho_data)
+        print('ut372: {} {} time: {}'.format(tacho_data[1], tacho_data[0], tacho_data[2]))
         db.add_record(tacho_data[2], 'ut372', tacho_data[1], tacho_data[0])
 
         c9c_data = pressure.read_data() if pressure.read_data() < 3500 else print('corrupt packet')
-        print(c9c_data)
-        db.add_record(tacho_data[2], 'c9c', c9c_data, 'N')
+        # print(c9c_data)
+        print('c9c  : {} gram'.format(c9c_data))
+        db.add_record(tacho_data[2], 'c9c', c9c_data, 'gram')
+
+        print('------------------------------------------')
